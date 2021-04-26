2a1cae34fd1122510df56cefc2d762da7684f447
Alfred Kishek <akishek22@gmail.com>
2021-04-26 19:37:29 -0400

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/akishek22/go-ml
+
+go 1.16
---
+++ linear_regression.go
@@ -0,0 +1,36 @@
+package main
+
+type LinearRegressionParams struct {
+}
+
+type LinearRegressionModel struct {
+	Weights   []float64
+	Intercept float64
+}
+
+type LinearRegression struct {
+	Params LinearRegressionParams
+	Model  LinearRegressionModel
+}
+
+func NewLinearRegression() LinearRegression {
+	return LinearRegression{}
+}
+
+func (lr *LinearRegression) Fit(x [][]float64, y []float64) {
+	// x's squared
+
+	// x1*y, x2*y... etc...
+	// x1 * x2
+
+}
+
+func (lr *LinearRegression) Predict(x [][]float64) {
+
+}
+
+func validate_input(x [][]float64, y []float64) error {
+	// x len must equal y len
+
+	return nil
+}
